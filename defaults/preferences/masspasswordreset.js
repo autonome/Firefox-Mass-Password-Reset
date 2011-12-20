fe0e3ea10661e017e9fb571cca535eb9fca6b690
Dietrich Ayala <autonome@gmail.com>
2011-12-20 12:35:46 -0800

    initial commit from http://hg.mozilla.org/users/jnightingale_mozilla.com/massreset/

---
+++ defaults/preferences/masspasswordreset.js
@@ -0,0 +1,2 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.masspasswordreset@johnathan.nightingale.description", "chrome://masspasswordreset/locale/masspasswordreset.properties");
